e15664e89f0814c80dc967a55820fd9405633406
bendazoo <abendezu385@gmail.com>
2024-02-13 16:16:50 -0500

    Added email and random password generator

--- src/Email.java
+++ src/Email.java
@@ -1,31 +1,47 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Email {
-    Scanner scanner = new Scanner(System.in);
+class Email {
     Random random = new Random();
     private String firstName;
     private String lastName;
     private String email;
-    private String password;
+    private String password = "";
+    final private String[] chars = {"1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 
-    public void setEmail(){
-        System.out.println("Please enter a firstname: ");
-        firstName = scanner.nextLine();
-        System.out.println("Please enter a lastname: ");
-        lastName = scanner.nextLine();
-        email = (firstName + "." + lastName + "@department.company.com").toLowerCase();
+
+
+    Email(String firstName, String lastName){
+
+        email = setEmail(firstName, lastName);
+        System.out.println(getEmail());
+        password = setPassword();
+        System.out.println(getPassword());
     }
-    public String getEmail(){
+
+    private String setEmail(String firstName, String lastName){
+        return (firstName + "." + lastName + "@department.company.com").toLowerCase();
+    }
+    private String getEmail()
+    {
         return "Your assigned email is: " + email;
     }
 
+    private String setPassword(){
+        int index;
+        int length = 0;
+        do{
+            index = random.nextInt(36);
+            password += chars[index];
+            length++;
 
-    public void setPassword(){
-        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        while()
+        }while(length != 10);
 
+        return password;
     }
 
+    private String getPassword(){
+        return "Your password is: " + password;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
-
-
-
-
-
-//       Email userEmail = new Email();
-//       userEmail.setEmail();
-//       System.out.println(userEmail.getEmail());
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Please enter a last name: ");
+        String lastName = scanner.nextLine();
+        scanner.close();
+        Email userEmail = new Email(firstName, lastName);
 
 
     }
